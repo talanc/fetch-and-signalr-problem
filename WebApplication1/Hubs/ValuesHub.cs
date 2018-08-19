ba7a3e1b841512cd21b67819f89fac49b526d600
Thomas Copeland <talan.copeland@gmail.com>
2018-08-19 14:35:03 +1000

    initial commit

---
+++ WebApplication1/Hubs/ValuesHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.Hubs
+{
+    public class ValuesHub : Hub
+    {
+        private readonly ValuesController values;
+
+        public ValuesHub(ValuesController values)
+        {
+            this.values = values;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return Clients.Caller.SendAsync("values", values.Get());
+        }
+    }
+}

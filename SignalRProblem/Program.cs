ce07d12205be6daada8faf7a4e4e0accfbff0121
Thomas Copeland <thomasc@s-cubed.com.au>
2018-08-21 15:06:23 +1000

    got it to fail with UseHttpSys

--- SignalRProblem/Program.cs
+++ SignalRProblem/Program.cs
@@ -19,6 +19,7 @@ namespace SignalRProblem
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseHttpSys();
     }
 }
